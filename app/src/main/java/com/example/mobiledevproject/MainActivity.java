25f5e204780162bc8f985abfbbaa4cc2ccf57b16
matin <matin.afzali.dev@gmail.com>
2026-06-08 04:10:44 -0800

    Add files

--- app/src/main/java/com/example/mobiledevproject/MainActivity.java
+++ app/src/main/java/com/example/mobiledevproject/MainActivity.java
@@ -1,8 +1,14 @@
+
 package com.example.mobiledevproject;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +17,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button website, call, go;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        website = findViewById(R.id.website);
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String patch = "https://soft98.ir";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(patch)));
+            }
         });
-
-        Button btnGoSecond = findViewById(R.id.btnGoSecond);
-
-        btnGoSecond.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, testActivity.class);
-            startActivity(intent);
+        call = findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel: 110"));
+                startActivity(intent);
+            }
+        });
+        go = findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), testActivity.class);
+                startActivity(intent);
+            }
         });
     }
+
+    ;
 }
